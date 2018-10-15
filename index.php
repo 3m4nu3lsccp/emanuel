be4fac2a3528788bac8d7b1adeed29e3b99cae72
Emanuel SCCP <es.parente@gmail.com>
2018-10-15 10:25:05 -0300

    mudei algumas coisas

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
     
-<<<<<<< HEAD
     echo"Olá Mundo";
     echo"Brasil Rumo a Direita";
-=======
-    echo"Bolsonaro 18";
-
-    echo"Olá Brasil"
->>>>>>> 123bb72839cea15226dcc8b242da48786651b121
 
 ?>
